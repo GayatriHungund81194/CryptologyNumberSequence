5e5227026a82dea29afaef82752c08792b7370ed
GayatriHungund81194 <gayatri811.gh@gmail.com>
2019-10-09 07:50:46 -0700

    Saving code

--- numberSequenceC.c
+++ numberSequenceC.c
@@ -51,7 +51,7 @@ int main()
 
       if (count3_1 == 3 || count3_1 == 4) {
             number = number + 3;
-            printf("Increment by 3 %d\n",&number);
+            printf("Increment by 3 %d\n",number);
             if (count3_1 == 4){
                count3_1 = 0;
             } 
@@ -63,7 +63,7 @@ int main()
       else if (((count4_1 == 1 || count4_1 == 6) && flag4 == 0) || ((count4_1 == 3 || count4_1 == 8) && flag4 == 1)) {
          if ((count4_1 == 1 || count4_1 == 6) && flag4 == 0) {
             number = number + 4;
-            printf("Increment by 4 %d\n",&number);
+            printf("Increment by 4 %d\n",number);
             if (count4_1 == 6) {
                count4_1 = 0;
                flag4 = 1;
@@ -71,7 +71,7 @@ int main()
          }
          else if ((count4_1 == 3 || count4_1 == 8) && flag4 == 1) {
             number = number + 4;
-            print("Increment by 4 %d\n",&number);
+            printf("Increment by 4 %d\n",number);
             if (count4_1 == 8) {
                count4_1 = 0;
             }
@@ -84,7 +84,7 @@ int main()
       else if (((count5_1 == 2 || count5_1 == 13) && flag5 == 0) || ((count5_1 == 5 || count5_1 == 16) && flag5 == 1)) {
          if ((count5_1 == 2 || count5_1 == 13) && flag5 == 0) {
             number = number + 5;
-            printf("Increment by 5 %d\n",&number);
+            printf("Increment by 5 %d\n",number);
             if (count5_1 == 13) {
                count5_1 = 0;
                flag5 = 1;
@@ -92,7 +92,7 @@ int main()
          }
          else if ((count5_1 == 5 || count5_1 == 16) && flag5 == 1) {
             number = number + 5;
-            printf("Increment by 5 %d\n",&number);
+            printf("Increment by 5 %d\n",number);
             if (count5_1 == 16) {
                count5_1 = 0;
             }
@@ -105,7 +105,7 @@ int main()
       else if (((count6_1 == 5 || count6_1 == 26) && flag6 == 0) || ((count6_1 == 11 || count6_1 == 32) && flag6 == 1)) {
          if ((count6_1 == 5 || count6_1 == 26) && flag6 == 0) {
             number = number + 6;
-            printf("Increment by 6 %d\n",&number);
+            printf("Increment by 6 %d\n",number);
             if (count6_1 == 26) {
                count6_1 = 0;
                flag6 = 1;
@@ -113,7 +113,7 @@ int main()
          }
          else if (((count6_1 == 11 || count6_1 == 32) && flag6 == 1)) {
             number = number + 6;
-            printf("Increment by 6 %d\n",&number);
+            printf("Increment by 6 %d\n",number);
             if (count6_1 == 32) {
                count6_1 = 0;
             }
@@ -126,7 +126,7 @@ int main()
       else if (((count7_1 == 10 || count7_1 == 53) && flag7 == 0) || ((count7_1 == 21 || count7_1 == 64) && flag7 == 1)) {
          if ((count7_1 == 10 || count7_1 == 53) && flag7 == 0) {
             number = number + 7;
-            printf("Increment by 7 ",number);
+            printf("Increment by 7 %d\n",number);
             if (count7_1 == 53) {
                count7_1 = 0;
                flag7 = 1;
@@ -134,7 +134,7 @@ int main()
          }
          else if (((count7_1 == 21 || count7_1 == 64) && flag7 == 1)) {
             number = number + 7;
-            printf("Increment by 7 %d\n",&number);
+            printf("Increment by 7 %d\n",number);
             if (count7_1 == 64) {
                count7_1 = 0;
             }
@@ -147,7 +147,7 @@ int main()
       else if (((count8_1 == 21 || count8_1 == 106) && flag8 == 0) || ((count8_1 == 43 || count8_1 == 128) && flag8 == 1)) {
          if ((count8_1 == 21 || count8_1 == 106) && flag8 == 0) {
             number = number + 8;
-            printf("Increment by 8 %d\n",&number);
+            printf("Increment by 8 %d\n",number);
             if (count8_1 == 106) {
                count8_1 = 0;
                flag8 = 1;
@@ -155,7 +155,7 @@ int main()
          }
          else if (((count8_1 == 43 || count8_1 == 128) && flag8 == 1)) {
             number = number + 8;
-            printf("Increment by 8 %d\n",&number);
+            printf("Increment by 8 %d\n",number);
             if (count8_1 == 128) {
                count8_1 = 0;
             }
@@ -168,7 +168,7 @@ int main()
       else if (((count9_1 == 42 || count9_1 == 213) && flag9 == 0) || ((count9_1 == 85 || count9_1 == 256) && flag9 == 1)){
          if ((count9_1 == 42 || count9_1 == 213) && flag9 == 0) {
             number = number + 9;
-            printf("Increment by 9 %d\n",&number);
+            printf("Increment by 9 %d\n",number);
             if (count9_1 == 213) {
                count9_1 = 0;
                flag9 = 1;
@@ -176,7 +176,7 @@ int main()
          }
          else if (((count9_1 == 95 || count9_1 == 256) && flag9 == 1)) {
             number = number + 9;
-            printf("Increment by 9 %d\n",&number);
+            printf("Increment by 9 %d\n",number);
             if (count9_1 == 256) {
                count9_1 = 0;
             }
@@ -186,19 +186,20 @@ int main()
       else if (((count10_1 == 85 || count10_1 == 426) && flag10 == 0) || ((count10_1 == 171 || count10_1 == 512) && flag10 == 1)) {
          if ((count10_1 == 85) && flag10 == 0) {
             number = number + 10;
-            printf("Increment by 10 %d\n",&number);
+            printf("Increment by 10 %d\n",number);
          }
          else if (((count10_1 == 171 || count10_1 == 512) && flag10 == 1)) {
             number = number + 10;
-            printf("Increment by 10 %d\n",&number);
+            printf("Increment by 10 %d\n",number);
             if (count10_1 == 512) {
                count10_1 = 0;
             }
          }
       }
+
       else {
          number = number + 11;
-         printf("Increment by 11 %d\n",&number);
+         printf("Increment by 11 %d\n",number);
          printf("X");
       }
    }
